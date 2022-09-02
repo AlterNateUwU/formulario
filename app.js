7201b30c92d77a44102d2df5a72eaae5b4255d44
AlterNateUwU <angelfabiano65@gmail.com>
2022-09-01 23:11:02 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+"user strict";
+
+const agregarTareaBTN = document.querySelector(".task-submit");
+const contenedorTareas = document.querySelector(".container-tasks");
+const formularioTexto = document.querySelector(".task-text");
+const tareasRestantes = document.getElementById("pending");
+
+let contadorDeID = 0;
+
+agregarTareaBTN.addEventListener("click", (e) => {
+
+  contadorDeID++;
+
+  crearElemento();
+
+  actualizarElementos();
+});
+
+const crearElemento = () => {
+  const NewTareaText = formularioTexto.value;
+
+  if (formularioTexto.value.length > 0) {
+    let tarea = document.createElement("DIV");
+    tarea.classList.add("task");
+    tarea.classList.add("flex");
+    tarea.id = `${contadorDeID}`;
+
+    let img = document.createElement("IMG");
+    img.src = "./img/trash-outline.svg";
+    tarea.appendChild(img);
+
+    let label = document.createElement("LABEL");
+    label.classList.add("flex");
+    tarea.appendChild(label);
+
+    let input = document.createElement("INPUT");
+    input.type = "checkbox";
+    label.appendChild(input);
+
+    let span = document.createElement("SPAN");
+    span.textContent = `${NewTareaText}`;
+    label.appendChild(span);
+
+    contenedorTareas.appendChild(tarea);
+
+    formularioTexto.value = "";
+  } else {
+    alert("No puede agregar tareas sin nombre 😠");
+  }
+};
+
+const actualizarElementos = () => {
+  let elementosTotales = contenedorTareas.querySelectorAll("DIV");
+  let checkboxsMarcados = contenedorTareas.querySelectorAll('input[type="checkbox"]:checked');
+
+  pending.textContent = `${elementosTotales.length - checkboxsMarcados.length}`;
+};
+
+const eliminarElemento = (id) => {
+  let eliminarPorID = document.getElementById(id);
+  contenedorTareas.removeChild(eliminarPorID);
+  actualizarElementos();
+};
+
+contenedorTareas.addEventListener("click", (evento) => {
+  if (evento.target.nodeName === "INPUT") {
+    actualizarElementos();
+  } else if (evento.target.nodeName === "IMG") {
+    eliminarElemento(evento.target.parentNode.id);
+  }
+});
